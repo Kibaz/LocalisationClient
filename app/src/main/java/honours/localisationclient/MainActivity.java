08c2597c1e18d085f2b47345946dfc620a335bf2
MarcusArcherCG <marcusarchercg@gmail.com>
2019-02-22 19:51:50 +0000

    Accelerometer configured and tested on test mobile device. Sending raw data to localisation server for processing.

--- app/src/main/java/honours/localisationclient/MainActivity.java
+++ app/src/main/java/honours/localisationclient/MainActivity.java
@@ -1,11 +1,14 @@
 package honours.localisationclient;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import networking.Client;
@@ -13,7 +16,14 @@ import networking.Client;
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     // Fields
+
+    // Views
     private GLView glView;
+    private TextView accelX;
+    private TextView accelY;
+    private TextView accelZ;
+
+    // Networking
     private Client client;
 
     /*
@@ -26,23 +36,83 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor accelerometer;
 
+    // Variables to store acceleration data
+    private float lastX, lastY, lastZ; // Store previous states
+
+    private float deltaX = 0; // Store change in X
+    private float deltaY = 0; // Store change in Y
+    private float deltaZ = 0; // Store change in Z
+
     // Activity initialisation method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        glView = (GLView) findViewById(R.id.glView);
+        // Initialise views
+        initViews();
 
         // Initialise client networking operations
+        initClientInstance();
+
+        // Initialise built-in sensor
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // Check if the device consists of the linear acceleration sensor type
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+        {
+            // Device has an accelerometer, proceed with accelerometer setup
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else
+        {
+            Log.d("Error: ", "This Device does not support the accelerometer");
+        }
+    }
+
+    private void initViews()
+    {
+        glView = (GLView) findViewById(R.id.glView);
+        accelX = (TextView) findViewById(R.id.accelX);
+        accelY = (TextView) findViewById(R.id.accelY);
+        accelZ = (TextView) findViewById(R.id.accelZ);
+    }
+
+    private void initClientInstance()
+    {
+        // Create client instance for communication with the localisation server
         client = new Client(8127,"192.168.1.7");
-        client.listen(); // Start listening for messages from the server
-        client.setMessage("test");
-        client.send();
+        client.listen(); // Listen for UDP packets sent from server
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+        setDefaultAcceleration();
+
+        displayAccelerationValues();
+
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        String messageToServer = "AccelX: " + deltaX + " AccelY: " + deltaY + " AccelZ: " + deltaZ;
+        client.setMessage(messageToServer);
+        client.send();
+
+    }
+
+    private void setDefaultAcceleration()
+    {
+        accelX.setText("0.0");
+        accelY.setText("0.0");
+        accelZ.setText("0.0");
+    }
+
+    private void displayAccelerationValues()
+    {
+        accelX.setText(Float.toString(deltaX));
+        accelY.setText(Float.toString(deltaY));
+        accelZ.setText(Float.toString(deltaZ));
     }
 
     @Override
